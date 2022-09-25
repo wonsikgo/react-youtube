0edf613c47ed4ec484669fae82485be3d4c73232
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-09-25 23:37:59 +0900

    유튜브 리스트 아이템 조회

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
 import "./app.css";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
-  return <h1>Hello :)</h1>;
+  const [videos, setVideos] = useState([]);
+  const [name, setName] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBYPS3vYonoWeIkqtq2LOMAibPKsnH2rEI",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  return (
+    <h1>
+      <VideoList videos={videos} />
+    </h1>
+  );
 }
 
 export default App;
---
+++ src/components/video_item/video_item.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const VideoItem = (props) => {
+  return <h1>{props.video.snippet.title}</h1>;
+};
+
+export default VideoItem;
