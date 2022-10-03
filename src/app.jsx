19a513f7dd20ee232c942c68b2638c74e3f200fa
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-10-03 23:58:16 +0900

    detail 데이터 바인딩

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,29 @@
 import { useEffect, useState } from "react";
 import styles from "./app.module.css";
+import VideoDetail from "./components/video_detail/video_detail";
 import VideoList from "./components/video_list/video_list";
 import VideoSearch from "./components/video_search/video_search";
 
 function App({ youtube }) {
+  const [video, setVideo] = useState(null);
   const [videos, setVideos] = useState([]);
-  const [name, setName] = useState([]);
+  const [isSelected, setIsSelected] = useState(false);
 
   const onSearch = (keyword) => {
-    console.log(keyword);
     youtube
       .search(keyword)
       .then((videos) => {
         console.log(videos);
+        setIsSelected(false);
         setVideos(videos);
       })
       .catch((error) => console.log("error", error));
+  };
+
+  const onVideoClick = (video) => {
+    console.log(video);
+    setVideo(video);
+    setIsSelected(true);
   };
 
   useEffect(() => {
@@ -25,12 +33,23 @@ function App({ youtube }) {
         setVideos(videos);
       })
       .catch((error) => console.log("error", error));
-  }, []);
+  }, [youtube]);
 
   return (
     <div className={styles.app}>
       <VideoSearch onSearch={onSearch} />
-      <VideoList videos={videos} />
+      <section lassName={styles.content}>
+        {isSelected && (
+          <div className={styles.detail}>
+            <VideoDetail video={video} />
+          </div>
+        )}
+        <VideoList
+          videos={videos}
+          onVideoClick={onVideoClick}
+          display={isSelected ? "list" : "grid"}
+        />
+      </section>
     </div>
   );
 }
--- src/components/video_detail/video_detail.jsx
+++ src/components/video_detail/video_detail.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styles from "./video_detail.module.css";
 
-const VideoDetail = () => {
-  return <div></div>;
-};
+const VideoDetail = ({ video, video: { snippet } }) => (
+  <section className={styles.detail}>
+    <iframe
+      className={styles.video}
+      type="text/html"
+      title="videoPlayer"
+      width="100%"
+      height="500px"
+      src={`https://www.youtube.com/embed/${video.id}`}
+      frameBorder="0"
+      allowFullScreen
+    ></iframe>
+    <h2>{snippet.title}</h2>
+    <h3>{snippet.channelTitle}</h3>
+    <pre className={styles.description}>{snippet.description}</pre>
+  </section>
+);
 
 export default VideoDetail;
