2848fb282eb3c95c0023f01cb487ff83a57fceca
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-10-04 23:21:12 +0900

    axios 추가

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app";
 import Youtube from "./service/youtube";
+import axios from "axios";
 
-const youtube = new Youtube();
+const httpClient = axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: { key: process.env.REACT_APP_APP_KEY },
+});
+
+const youtube = new Youtube(httpClient);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/service/youtube.js
+++ src/service/youtube.js
@@ -1,28 +1,29 @@
 class Youtube {
-  constructor(key) {
-    this.key = key;
-    this.getRequestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
+  constructor(httpClient) {
+    this.youtube = httpClient;
   }
 
   async mostPopular() {
-    const response = await fetch(
-      `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${process.env.REACT_APP_APP_KEY}`,
-      this.getRequestOptions
-    );
-    const result_1 = await response.json();
-    return result_1.items;
+    const params = {
+      part: "snippet",
+      chart: "mostPopular",
+      maxResults: 25,
+    };
+    const { data } = await this.youtube.get("/videos", { params });
+
+    return data.items;
   }
 
   async search(query) {
-    const response = await fetch(
-      `https://www.googleapis.com/youtube/v3/search?q=${query}&part=snippet&maxResults=25&key=${process.env.REACT_APP_APP_KEY}`,
-      this.getRequestOptions
-    );
-    const result_1 = await response.json();
-    return result_1.items.map((item) => {
+    const params = {
+      part: "snippet",
+      maxResults: 25,
+      type: "video",
+      q: query,
+    };
+    const { data } = await this.youtube.get("/search", { params });
+
+    return data.items.map((item) => {
       return {
         ...item,
         id: item.id.videoId,
